3b8b6aa8547d5d4e1131183dd377b1b22e13be0a
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-08-05 22:24:40 -0300

    font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,20 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins, DM_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['400', '500', '700'],
+  display: 'swap',
+})
+
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  variable: '--font-dm-sans',
+  weight: ['400', '500', '700'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={`${poppins.variable} ${dmSans.variable}`}>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1 className="font-title text-3xl font-bold">Hello World!</h1>
+      <p className="font-text text-base">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magnam
+        voluptatem est cupiditate accusantium aliquid non veritatis magni
+        officia vel autem rem omnis natus id minima dolores, eius rerum.
+        Dignissimos!
+      </p>
+    </>
   )
 }
